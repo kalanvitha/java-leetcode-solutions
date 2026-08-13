64867302836b4304bb933303ac9f33acc7a0d8a4
kalanvitha Gunurathi <kalanvithagundrathi@gmail.com>
2026-08-13 15:35:33 +0530

    Time: 27 ms (94.71%), Space: 143.4 MB (89.98%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,20 @@
+     class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        //create deque 
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        for(int i=0; i<n; i++){
+            //check window size 
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.pollFirst();
+            }
+          while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
+              dq.pollLast();
+          }
+          dq.addLast(i);
+          if(i>=k-1) res[i-k+1] = nums[dq.peekFirst()]; 
+          }
+        return res;
+    }
+}
