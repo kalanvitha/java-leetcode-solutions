b95fd0d94f9376fde4f8dbabb54cd3622d254355
kalanvitha Gunurathi <kalanvithagundrathi@gmail.com>
2026-09-08 18:32:22 +0530

    Time: 3 ms (21.44%), Space: 48.3 MB (76.56%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if(root==null) return res;
+        //bfs method (level by level) = queue 
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        //process the queue 
+        while(!q.isEmpty()){
+            int size = q.size();
+            long sum=0;
+            for(int i=0; i<size; i++){
+                TreeNode temp = q.pop();
+                sum += temp.val;
+                //check for child 
+                if(temp.left!=null) q.offer(temp.left);
+                if(temp.right!=null) q.offer(temp.right);
+            }
+            res.add((double)sum/size);
+        }
+        return res;
+    }
+}
